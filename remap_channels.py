497b08fabd25d1150088da4cadd6fd2f63810616
Wojciech Kedzierski <wojked@wojked.pl>
2019-08-03 22:07:37 +0200

    Fixing channels.

--- remap_channels.py
+++ remap_channels.py
@@ -1,4 +1,7 @@
+import argparse
 import os
+import shutil
+import urllib.request
 
 # This is something that needs to be configured by you
 # CHANNEL_ID --> CHANNEL_NUMBER (must be int in string)
@@ -11,14 +14,14 @@ transformations = [
     "TVN",
     "TV 4",
     "TV Puls",
-    "TVN7 HD",
+    "TVN 7 HD",
     "TVN 7",
-    "PULS 2",
+    "TV Puls 2",
     "TV6",
     "Super Polsat",
     "ESKA TV",
     "TTV HD",
-    "TTV", 
+    "TTV",
     "Polo TV",
     "ATM Rozrywka",
     "TV Trwam",
@@ -28,12 +31,19 @@ transformations = [
     "TVP Kultura",
     "TVP Historia",
     "TVP Sport",
-    "TVP Info"
+    "TVP Info",
 ]
 
-def rename():
-    file_in = "epg.xml"
-    file_out = "epg-fixed.xml"
+
+def download(url, file_name):
+    # Download the file from `url` and save it locally under `file_name`:
+    with urllib.request.urlopen(url) as response, open(file_name, "wb") as out_file:
+        shutil.copyfileobj(response, out_file)
+
+    print(f"Downloaded {url} as {file_name}")
+
+
+def rename_remap(file_in, file_out):
     with open(file_in, "rt") as fin:
         with open(file_out, "wt") as fout:
             for line in fin:
@@ -45,7 +55,7 @@ def rename():
                     input_string = f'="{channel_name}"'
                     # Add numbering
                     output_number = i + 1
-                    output_string = f'="{output_number}{channel_name}"'
+                    output_string = f'="0{output_number}{channel_name}"'
                     # Remove spaces
                     output_string = output_string.replace(" ", "")
 
@@ -53,6 +63,36 @@ def rename():
 
                 fout.write(fixed_line)
 
+    print(f"Remapped {file_in} to {file_out}")
 
-if __name__ == '__main__':
-    rename()
+
+def upload(filename, target_path):
+    destination_filename = os.path.join(target_path, filename)
+    shutil.copyfile(filename, destination_filename)
+
+    print(f"Uploaded {filename} to {destination_filename}")
+
+
+if __name__ == "__main__":
+    """ 
+    Usage example: 
+        python remap_channels.py https://URL/guide.xml epg.xml epg-fixed.xml /network/tvdir/
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="source of the XML EPG")
+    parser.add_argument("epg_file_name", help="desired downloaded file name")
+    parser.add_argument(
+        "epgfixed_file_name", help="desired target name of the XML file"
+    )
+    parser.add_argument("target_path", help="place on the server")
+    args = parser.parse_args()
+
+    # print(args.url)
+    # print(args.epg_file_name)
+    # print(args.epgfixed_file_name)
+    # print(args.target_path)
+
+    download(url=args.url, file_name=args.epg_file_name)
+    rename_remap(file_in=args.epg_file_name, file_out=args.epgfixed_file_name)
+    upload(filename=args.epgfixed_file_name, target_path=args.target_path)

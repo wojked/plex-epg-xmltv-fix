302467b3131bd1cc6f051be3b882d7a3bc199678
Wojciech Kedzierski <wojked@wojked.pl>
2019-08-04 17:54:01 +0200

    Remove f"strings"

--- remap_channels.py
+++ remap_channels.py
@@ -42,7 +42,9 @@ def download(url, file_name):
     with urllib.request.urlopen(url) as response, open(file_name, "wb") as out_file:
         shutil.copyfileobj(response, out_file)
 
-    print(f"Downloaded {url} as {file_name}")
+    print(
+        "Downloaded {url} as {file_name}".format({"url": url, "file_name": file_name})
+    )
 
 
 def fix_channel_name(channel_name):
@@ -73,7 +75,13 @@ def patch_channel_map(channel_map, forced_channel_map=None, prefix="0"):
 
         if channel_initial_name in forced_channel_map.keys():
             forced_channel_id = forced_channel_map.get(channel_initial_name, "")
-            patched_channel_name = f"{prefix}{forced_channel_id}{patched_channel_name}"
+            patched_channel_name = "{prefix}{forced_channel_id}{patched_channel_name}".format(
+                {
+                    "prefix": prefix,
+                    "forced_channel_id": forced_channel_id,
+                    "patched_channel_name": patched_channel_name,
+                }
+            )
 
         patched_channel_map[channel_initial_name] = patched_channel_name
 
@@ -98,12 +106,19 @@ def rename_remap(file_in, file_out, forced_channel_map=None):
                 fixed_line = line
                 for channel_name, fixed_channel_name in channel_map.items():
                     fixed_line = fixed_line.replace(
-                        f'="{channel_name}"', f'="{fixed_channel_name}"'
+                        '="{channel_name}"'.format({"channel_name": channel_name}),
+                        '="{fixed_channel_name}"'.format(
+                            {"fixed_channel_name": fixed_channel_name}
+                        ),
                     )
 
                 fout.write(fixed_line)
 
-    print(f"Remapped {file_in} to {file_out}")
+    print(
+        "Remapped {file_in} to {file_out}".format(
+            {"file_in": file_in, "file_out": file_out}
+        )
+    )
 
 
 def upload(filename, target_path):
@@ -113,7 +128,11 @@ def upload(filename, target_path):
     destination_filename = os.path.join(target_path, filename)
     shutil.copyfile(filename, destination_filename)
 
-    print(f"Uploaded {filename} to {destination_filename}")
+    print(
+        "Uploaded {filename} to {destination_filename}".format(
+            {"filename": filename, "destination_filename": destination_filename}
+        )
+    )
 
 
 def upload_ftp(
@@ -125,12 +144,23 @@ def upload_ftp(
         ftp.dir()
 
         with open(filename, "rb") as fp:
-            cmd = f"STOR {destination_filename}"
+            cmd = "STOR {destination_filename}".format(
+                {"destination_filename": destination_filename}
+            )
             ftp.storbinary(cmd, fp)
 
         ftp.close()
 
-    print(f"Uploaded {filename} to FTP: {ftp_address}/{destination_dir}/{destination_filename}")        
+    print(
+        "Uploaded {filename} to FTP: {ftp_address}/{destination_dir}/{destination_filename}".format(
+            {
+                "filename": filename,
+                "ftp_address": ftp_address,
+                "destination_dir": destination_dir,
+                "destination_filename": {destination_filename},
+            }
+        )
+    )
 
 
 if __name__ == "__main__":
@@ -140,25 +170,25 @@ if __name__ == "__main__":
     """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("config_filename", help="config JSON file name")      
+    parser.add_argument("config_filename", help="config JSON file name")
     args = parser.parse_args()
-    
+
     with open(args.config_filename, "r") as config_json:
-        config = json.load(config_json)    
+        config = json.load(config_json)
 
-        epg = config['epg']
-        ftp_creds = config['ftp']        
+        epg = config["epg"]
+        ftp_creds = config["ftp"]
 
-        download(url=epg['source'], file_name=epg['local_filename'])
+        download(url=epg["source"], file_name=epg["local_filename"])
 
         rename_remap(
-            file_in=epg['local_filename'],
-            file_out=epg['fixed_local_filename'],
-            forced_channel_map=config['channels'],
+            file_in=epg["local_filename"],
+            file_out=epg["fixed_local_filename"],
+            forced_channel_map=config["channels"],
         )
 
         upload_ftp(
-            filename=epg['fixed_local_filename'],
+            filename=epg["fixed_local_filename"],
             ftp_address=ftp_creds["address"],
             username=ftp_creds["username"],
             password=ftp_creds["password"],
